b4b977e1bbeb4e2733c1446aa23cdb8e822933a1
Rae Stanton <raestanton93@gmail.com>
2023-10-05 12:38:23 -0700

    CA fin -UD

--- server/app.py
+++ server/app.py
@@ -19,11 +19,11 @@ api = Api(app)
 class Home(Resource):
 
     def get(self):
-        
+
         response_dict = {
             "message": "Welcome to the Newsletter RESTful API",
         }
-        
+
         response = make_response(
             response_dict,
             200,
@@ -31,12 +31,13 @@ class Home(Resource):
 
         return response
 
+
 api.add_resource(Home, '/')
 
 class Newsletters(Resource):
 
     def get(self):
-        
+
         response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
 
         response = make_response(
@@ -47,7 +48,7 @@ class Newsletters(Resource):
         return response
 
     def post(self):
-        
+
         new_record = Newsletter(
             title=request.form['title'],
             body=request.form['body'],
@@ -76,6 +77,39 @@ class NewsletterByID(Resource):
         response = make_response(
             response_dict,
             200,
+        )
+
+        return response
+
+    def patch(self, id):
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+
+    def delete(self, id):
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "Newsletter deleted"}
+
+        response = make_response(
+            response_dict,
+            200
         )
 
         return response
